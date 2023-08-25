9135052f8e4b8ae17e601c699555cd5fc74ba455
kylq <kylq@vtn-global.com>
2023-08-25 16:21:08 +0700

    io

--- lib/src/shared/cubit/non_nullable_cubit.dart
+++ lib/src/shared/cubit/non_nullable_cubit.dart
@@ -1,8 +1,9 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:injectable/injectable.dart';
 
-@injectable
-class NonNullableValueCubit<T> extends Cubit<T> {
+import '../io/base_input.dart';
+
+class NonNullableValueCubit<T extends BaseInput> extends Cubit<T> {
   NonNullableValueCubit(@factoryParam super.initialState);
 
   void change(T value) => emit(value);
--- lib/src/shared/cubit/nullable_cubit.dart
+++ lib/src/shared/cubit/nullable_cubit.dart
@@ -1,8 +1,9 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:injectable/injectable.dart';
 
-@injectable
-class NullableValueCubit<T> extends Cubit<T?> {
+import '../io/base_input.dart';
+
+class NullableValueCubit<T extends BaseInput> extends Cubit<T?> {
   NullableValueCubit(@factoryParam super.initialState);
 
   void change(T? value) => emit(value);
