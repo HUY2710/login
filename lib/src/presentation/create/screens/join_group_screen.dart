326a96b7ddbe7fac4111644462058dd94b23e047
KyLQ <kylq@vtn-global.com>
2024-02-03 15:46:11 +0700

    update join group and marker

---
+++ lib/src/presentation/create/screens/join_group_screen.dart
@@ -0,0 +1,162 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:pin_code_fields/pin_code_fields.dart';
+
+import '../../../config/di/di.dart';
+import '../../../config/navigation/app_router.dart';
+import '../../../gen/gens.dart';
+import '../../../shared/extension/context_extension.dart';
+import '../../../shared/widgets/custom_appbar.dart';
+import '../../home/cubit/validate_code/code_validation_cubit.dart';
+import '../../home/home_screen.dart';
+
+@RoutePage()
+class JoinGroupScreen extends StatelessWidget {
+  JoinGroupScreen({super.key});
+  final CodeValidationCubit codeValidCubit = getIt<CodeValidationCubit>();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: const CustomAppBar(),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 40.w),
+                child: Text(
+                  'Joining a group? Enter your invite code',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: MyColors.black34,
+                    fontSize: 20.sp,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+              ),
+              SizedBox(
+                width: 24.r,
+                height: 24.r,
+              ),
+              60.verticalSpace,
+              Text(
+                context.l10n.joinGroupContent,
+                style: TextStyle(
+                    fontSize: 16.sp,
+                    fontWeight: FontWeight.w500,
+                    letterSpacing: -0.4,
+                    color: MyColors.black34),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 24),
+                child: PinCodeTextField(
+                  appContext: context,
+                  pastedTextStyle: TextStyle(
+                    color: Colors.green.shade600,
+                    fontWeight: FontWeight.bold,
+                  ),
+                  textStyle: TextStyle(
+                      fontSize: 32.sp,
+                      fontWeight: FontWeight.w600,
+                      color: MyColors.black34),
+                  length: 6,
+
+                  blinkWhenObscuring: true,
+                  animationType: AnimationType.fade,
+                  textCapitalization: TextCapitalization.characters,
+                  pinTheme: PinTheme(
+                      inactiveColor: MyColors.secondPrimary,
+                      activeColor: MyColors.secondPrimary,
+                      shape: PinCodeFieldShape.underline,
+                      borderRadius: BorderRadius.circular(50),
+                      fieldHeight: 50.h,
+                      fieldWidth: 32.w,
+                      activeFillColor: Colors.transparent,
+                      selectedColor: MyColors.primary,
+                      inactiveFillColor: Colors.transparent,
+                      selectedFillColor: Colors.transparent,
+                      activeBorderWidth: 4.r,
+                      borderWidth: 4.r,
+                      inactiveBorderWidth: 4.r,
+                      selectedBorderWidth: 4.r,
+                      disabledBorderWidth: 4.r),
+                  cursorColor: Colors.black,
+                  animationDuration: const Duration(milliseconds: 300),
+                  enableActiveFill: true,
+                  // errorAnimationController: errorController,
+                  // controller: textEditingController,
+                  keyboardType: TextInputType.text,
+                  boxShadows: const [
+                    BoxShadow(
+                      offset: Offset(0, 1),
+                      color: Colors.black12,
+                      blurRadius: 10,
+                    )
+                  ],
+                  onCompleted: (code) async {
+                    debugPrint('Completed');
+                    EasyLoading.show();
+                    await codeValidCubit.submit(code, context);
+                    EasyLoading.dismiss();
+                  },
+                  // onTap: () {
+                  //   print("Pressed");
+                  // },
+                  onChanged: (value) {
+                    debugPrint(value);
+                    // setState(() {
+                    //   currentText = value;
+                    // });
+                  },
+                  beforeTextPaste: (text) {
+                    debugPrint('Allowing to paste $text');
+                    //if you return true then it will show the paste confirmation dialog. Otherwise if false, then nothing will happen.
+                    //but you can show anything you want here, like your pop up saying wrong paste format or etc
+                    return true;
+                  },
+                ),
+              ),
+              BlocConsumer<CodeValidationCubit, CodeValidationState>(
+                bloc: codeValidCubit,
+                listener: (context, state) {
+                  state.maybeWhen(
+                      orElse: () {},
+                      valid: (group) {
+                        Fluttertoast.showToast(msg: 'Join group success');
+                        context.popRoute();
+                        getIt<AppRouter>().replaceAll([const HomeRoute()]);
+                      });
+                },
+                builder: (context, state) {
+                  return BlocBuilder<CodeValidationCubit, CodeValidationState>(
+                    bloc: codeValidCubit,
+                    builder: (context, state) {
+                      return state.maybeWhen(
+                        orElse: () => SizedBox(height: 30.h),
+                        inValid: (error) {
+                          return Padding(
+                            padding: EdgeInsets.symmetric(vertical: 20.h),
+                            child: Text(error,
+                                style: const TextStyle(
+                                  color: Colors.red,
+                                )),
+                          );
+                        },
+                      );
+                    },
+                  );
+                },
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
